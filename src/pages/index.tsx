bb9403b374796f52bed3d2b0f4a96a97a4b97236
Emmanuel Ma <emmanuel.ma@gmail.com>
2024-04-19 04:11:09 -0400

    fix broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/Overview"
           >
-            Check it out 🦧
+            Check it out 📚
           </Link>
         </div>
       </div>
